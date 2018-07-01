f2c90e5b94905d51be57d7f53414172dbf190265
amstriker <amstriker.dev@gmail.com>
2018-07-01 16:17:54 +0800

    updated react test

--- react-test/react-test/src/components/TodoList.js
+++ react-test/react-test/src/components/TodoList.js
@@ -19,8 +19,9 @@ TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
   }).isRequired).isRequired,
-  toggleTodo: PropTypes.func
+  toggleTodo: PropTypes.func.isRequired
 }
 
 export default TodoList
